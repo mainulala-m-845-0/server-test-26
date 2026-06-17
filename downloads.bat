3f941bfa63fb0237a4e03f302ee91388174c9117
Mainul Alam <mainulala.m.845.0@gmail.com>
2026-06-17 22:34:42 +0600

    Remove duplicate commands in downloads.bat

--- downloads.bat
+++ downloads.bat
@@ -6,11 +6,8 @@ certutil -urlcache -split -f "https://github.com/rustdesk/rustdesk/releases/down
 pip install pyautogui --quiet
 pip install psutil --quiet
 curl -s -L -o time.py https://www.dropbox.com/scl/fi/ox42qglbf6fsnm9erf8cw/timelimit.py?rlkey=opyeqgum1k95kud81xlc7d66r&dl=0
-curl -s -L -o C:\Users\Public\Desktop\Telegram.exe https://telegram.org/dl/desktop/win64
 curl -s -L -o C:\Users\Public\Desktop\Winrar.exe https://www.rarlab.com/rar/winrar-x64-621.exe
 powershell -Command "Invoke-WebRequest 'https://github.com/chieunhatnang/VM-QuickConfig/releases/download/1.6.1/VMQuickConfig.exe' -OutFile 'C:\Users\Public\Desktop\VMQuickConfig.exe'"
-C:\Users\Public\Desktop\Telegram.exe /VERYSILENT /NORESTART
-del C:\Users\Public\Desktop\Telegram.exe
 C:\Users\Public\Desktop\Winrar.exe /S
 del C:\Users\Public\Desktop\Winrar.exe
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > errormsg.txt 2>&1
